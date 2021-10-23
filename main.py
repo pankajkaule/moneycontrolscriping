14337d8e8a99f24858e03a2631fef9b306676786
pankajkaule <pankajkaule@pankajkaule@gmail.com>
2021-10-23 13:25:16 +0530

    lxml added

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
-data = {
-    'mth_frm_mth': '01',
-    'mth_frm_yr': '2019',
-    'mth_to_mth': '01',
-    'mth_to_yr': '2020',
-    'hdn': 'monthly'
-}
-
-url = 'https://www.moneycontrol.com/stocks/marketstats/nse-gainer/all-companies_-2/'
-soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+# url = 'https://www.moneycontrol.com/stocks/marketstats/nse-gainer/all-companies_-2/'
+# soup = BeautifulSoup(requests.get(url).content, 'html.parser').text
 
 
-print(soup.div, class_="bsr_table hist_tbl_hm")
+# allCompanies = soup.findall('div', class_='bsr_table hist_tbl_hm')
 # all_data = []
 # for tr in soup.select('.bsr_table hist_tbl_hm'):
 #     print(tr)
@@ -38,3 +30,91 @@ print(soup.div, class_="bsr_table hist_tbl_hm")
 
 
 # print(datatable)  # to print html in tree structure
+
+
+data = requests.get(
+    'https://www.moneycontrol.com/stocks/marketstats/bsemact1/index.php').text
+
+
+data1 = requests.get(
+    'https://www.moneycontrol.com/stocks/marketstats/nse-gainer/all-companies_-2/').text
+
+soup = BeautifulSoup(data1, 'lxml')
+
+
+# print(soup)
+allCompanies = soup.find_all('span', class_='gld13 disin')
+
+# print(allCompanies)
+
+companyNames = []
+for row in allCompanies:
+    link = row.find('a').text
+    companyNames.append(link)
+
+# print(companyNames)
+
+tablerow = soup.find_all('tr')
+
+companyHigh = []
+companyLow = []
+companyClose = []
+companyChange = []
+for tr in tablerow:
+
+    # high price
+    high = tr.find_all('td', attrs={'width': 75, 'align': 'right'})
+    if len(high) == 0:
+        continue
+    for i in high:
+        i = i.text.replace(',', '')
+        companyHigh.append(float(i))
+        break
+# print(companyHigh)
+#         # low price
+    low = tr.find_all('td', attrs={'width': 80, 'align': 'right'})
+    for i in low:
+        i = i.text.replace(',', '')
+        print(i)
+        companyLow.append(float(i))
+
+#         # close price
+
+    close = tr.find_all('td', attrs={'width': 85, 'align': 'right'})
+
+    # print(close)
+    for i in close:
+        i = i.text.replace(',', '')
+        companyClose.append(i)
+
+    change = tr.find_all('td', attrs={'width': 80, 'align': 'right'})
+    # print(change)
+
+    for i in change:
+        if i.has_attr('class'):
+            companyChange.append(float(i.text))
+
+companyData = []
+# print(companyChange)
+# print(len(companyClose))
+# print(len(companyHigh))
+print(len(companyLow))
+# print(len(companyClose))
+# print(len(companyChange))
+
+
+# for i in range(len(companyNames)):
+#     companyData.append({
+#         'company': companyNames[i],
+#         'high': companyHigh[i],
+#         'low': companyLow[i],
+#         'change_in_per': companyChange[i],
+#         'close': companyClose[i]
+#     })
+
+# print(companyData)
+# new_dict = sorted(
+
+#     companyData, key=lambda i: i['change_in_per'], reverse=True
+# )
+# print(new_dict)
